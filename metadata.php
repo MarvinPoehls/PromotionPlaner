8e10650b6b10103244a85bbaf4ef739bc223ec70
MarvinPoehls <112632120+MarvinPoehls@users.noreply.github.com>
2023-03-28 18:26:47 +0200

    Add files via upload

---
+++ metadata.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.0';
+
+/**
+ * Module information
+ */
+$aModule = [
+    'id'           => 'promotionplaner',
+    'title'  => [
+        'de' => 'Promotion-Planer',
+        'en' => 'Promotion-Planer',
+    ],
+    'description'  => [
+        'de' => 'Modul mit dem man zeitlich gesteuerte Werbebanner auf bestimmten Seiten anlegen kann.',
+        'en' => 'Module with which you can create time-controlled advertising banners on certain pages.',
+    ],
+    'thumbnail'    => '',
+    'version'      => '1.0',
+    'author'       => 'Marvin Poehls',
+    'url'          => 'https://www.fatchip.de/',
+    'email'        => 'marvin.poehls@fatchip.de',
+    'extend'       => [],
+    'blocks' => [],
+    'settings' => [],
+];
