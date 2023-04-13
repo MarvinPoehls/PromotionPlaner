c9d9d615901882a0415001275c6f4457806e33c5
MarvinPoehls <112632120+MarvinPoehls@users.noreply.github.com>
2023-04-13 15:56:04 +0200

    Add files via upload

---
+++ views/admin/en/fc_promotionplaner_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$sLangName = 'English';
+
+$aLang = [
+    'charset'   => 'UTF-8',
+    'FC_PROMOTIONPLANER_URL' => 'Banner',
+    'HELP_FC_PROMOTIONPLANER_URL_ARTICLE' => 'Upload your banner here. This banner is displayed on the article page. Leave blank to remove banner. Warning: This banner will overwrite the manufacturer banner',
+    'HELP_FC_PROMOTIONPLANER_URL_CATEGORY' => 'Upload your banner here. This banner is displayed on the category page. Leave blank to remove banner',
+    'HELP_FC_PROMOTIONPLANER_URL_MANUFACTURER' => 'Upload your banner here. This banner is displayed on every article page from this manufacturer. Leave blank to remove banner. Warning: This banner is overwritten by individual article banners',
+    'FC_PROMOTIONPLANER_FROM' => 'From',
+    'HELP_FC_PROMOTIONPLANER_FROM' => 'The start date from when the banner will be displayed',
+    'FC_PROMOTIONPLANER_TO' => 'To',
+    'HELP_FC_PROMOTIONPLANER_TO' => 'The end date until when the banner will be displayed',
+    ];
---
+++ views/blocks/promotionplaner_banner_articles.tpl
@@ -0,0 +1,8 @@
+[{if $oDetailsProduct->isPromotionTime()}]
+<div>
+    <img style="width: 100%; object-fit: cover; filter: blur(4px) brightness(80%); margin-bottom: 5%;" src="[{$oDetailsProduct->getBannerUrl()}]" height="65" alt="Promotion Banner">
+    <img style="width: 95%; object-fit: contain; position: absolute; left: 2.5%;" src="[{$oDetailsProduct->getBannerUrl()}]" height="65">
+</div>
+[{/if}]
+
+[{$smarty.block.parent}]
